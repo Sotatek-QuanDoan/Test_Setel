3a64829d15bb727dd1f4fd113e7aeede8b0c889f
Quan Ta <quan.ta@sotatek.com>
2020-12-10 21:37:11 +0700

    style: husky for format and lint

--- payment-app/.eslintrc.js
+++ payment-app/.eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     semi: ['error', 'always'],
+    '@typescript-eslint/no-unused-vars': 'off',
   },
-  'no-unused-vars': ['error', { vars: 'all' }],
 };
